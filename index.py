0385331c47c112da9b24b1096beda26285a73d6f
jpalanca <jpalanca@gmail.com>
2023-02-20 20:06:18 +0100

    Added some improvements and the possibility to review solution before download.

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 # Flask application
+import copy
 import os
 import random
 import string
 
 from flask import Flask, render_template, request, redirect, flash, send_from_directory
+from pretty_html_table import build_table
 from werkzeug.utils import secure_filename
 
-from main import read_df, generate_questions, solve_hours, check_hours
+from main import read_df, generate_questions, solve_hours, check_hours, transform_df_to_str_types
 
 app = Flask(__name__)
 
@@ -19,76 +21,99 @@ ALLOWED_EXTENSIONS = {'csv'}
 
 
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # View to ask for CSV file
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/config', methods=['GET', 'POST'])
 def config():
-	if request.method == 'POST':
-		# check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-			df, working_days, num_working_days = read_df(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			questions = generate_questions(df)
+            df, working_days, num_working_days = read_df(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            questions = generate_questions(df)
+            period = working_days[0] + ' - ' + working_days[-1]
 
-			return render_template('config.html', questions=questions, filename=filename)
+            return render_template('config.html', questions=questions, period=period, filename=filename)
 
 
 @app.route('/solve', methods=['GET', 'POST'])
 def solve():
-	if request.method == 'POST':
-		# Get form data
-		form_data = request.form
-		# Get filename
-		filename = form_data['filename']
-		# Get hours by project
-		hours_by_project = {}
+    if request.method == 'POST':
+        # Get form data
+        form_data = request.form
+        # Get filename
+        filename = form_data['filename']
+        # Get hours by project
+        hours_by_project = {}
 
-		for key, value in form_data.items():
-			if key not in ['filename', 'submit']:
-				project = key.replace("(", "").replace(")", "").replace("'", "").replace('"', "")
-				name, hours = project.split(",")
-				project = (name, float(hours))
-				if project[0] == "__teaching__":
-					teaching_hours = float(value)
-				else:
-					hours_by_project[project] = float(value)
-		# Solve hours
-		df, working_days, num_working_days = read_df(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        for key, value in form_data.items():
+            if key not in ['filename', 'submit']:
+                project = key.replace("(", "").replace(")", "").replace("'", "").replace('"', "")
+                name, hours = project.split(",")
+                project = (name, float(hours))
+                if project[0] == "__teaching__":
+                    teaching_hours = float(value)
+                elif project[0] == "__empty__":
+                    hours_by_project[project] = value
+                else:
+                    hours_by_project[project] = float(value)
+        # Solve hours
+        df, working_days, num_working_days = read_df(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-		# Check hours
-		try:
-			checked_df = check_hours(df, working_days, num_working_days, teaching_hours, hours_by_project)
-		except Exception as e:
-			msgs = str(e).split("\n")
-			return render_template('error.html', error=msgs)
+        # Check hours
+        try:
+            checked_df = check_hours(df, working_days, num_working_days, teaching_hours, hours_by_project)
+        except Exception as e:
+            msgs = str(e).split("\n")
+            return render_template('error.html', error=msgs)
 
-		solved_df = solve_hours(checked_df, hours_by_project, working_days)
+        solved_df = solve_hours(checked_df, hours_by_project, working_days)
 
-		# Save to CSV with random name
-		random_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-		solved_df.to_csv(os.path.join("results", f'solved_{random_id}.csv'), index=False, sep=";", encoding='iso-8859-1')
+        float_df = copy.deepcopy(solved_df)
 
-		# return file download
-		return send_from_directory("results", f'solved_{random_id}.csv', as_attachment=True)
+        solved_df = transform_df_to_str_types(solved_df, working_days)
+
+        # Save to CSV with random name
+        random_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+        solved_df.to_csv(os.path.join("results", f'solved_{random_id}.csv'), index=False, sep=";", encoding='iso-8859-1')
+
+        # Delete column from df
+        float_df = float_df.drop(columns=['DNI', "Nombre", "Clave específica", "Id Actividad"])
+        # sum row from 7th row to end
+        #float_df['Horas Totales'] = float_df.iloc[3:].sum()
+        float_df['Horas Totales'] = float_df.loc[:, float_df.columns[3:]].sum(axis=1)
+        # reorder column
+        columns = ["Proyecto", "Actividad", "Working Package", "Horas Totales"]
+        float_df = float_df[columns + [col for col in float_df.columns if col not in columns]]
+        # Build table
+        html_table = build_table(float_df, 'blue_light')
+
+        return render_template('result.html', df=html_table, random_id=random_id)
+
+
+@app.route('/download/<random_id>', methods=['GET'])
+def download(random_id):
+    # return file download
+    return send_from_directory("results", f'solved_{random_id}.csv', as_attachment=True)
 
 
 # run Flask app
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- main.py
+++ main.py
@@ -8,121 +8,132 @@ MAX_HOUR_DAY = 7.5
 
 
 def get_working_days(_first_date, _last_date):
-	"""Returns a list of the working days in a range of dates"""
-	_working_days = []
-	for date in pd.date_range(_first_date, _last_date):
-		if date.dayofweek < 5:
-			_working_days.append(date.strftime("%-d/%-m/%y"))
-	return _working_days
+    """Returns a list of the working days in a range of dates"""
+    _working_days = []
+    for date in pd.date_range(_first_date, _last_date):
+        if date.dayofweek < 5:
+            _working_days.append(date.strftime("%-d/%-m/%y"))
+    return _working_days
 
 
 def read_df(_filename):
-	_df = pd.read_csv(_filename, encoding='iso-8859-1', sep=';')
-	first_day, first_month, first_year = _df.columns[7].split('/')
-	first_date = datetime.date(year=int(first_year) + 2000, month=int(first_month), day=int(first_day))
-	last_day, last_month, last_year = _df.columns[-1].split('/')
-	last_date = datetime.date(year=int(last_year) + 2000, month=int(last_month), day=int(last_day))
-	_working_days = get_working_days(first_date, last_date)
-	_num_working_days = len(_working_days)
+    _df = pd.read_csv(_filename, encoding='iso-8859-1', sep=';')
+    first_day, first_month, first_year = _df.columns[7].split('/')
+    first_year = '20' + first_year if len(first_year) == 2 else first_year
+    first_date = datetime.date(year=int(first_year), month=int(first_month), day=int(first_day))
+    last_day, last_month, last_year = _df.columns[-1].split('/')
+    last_year = '20' + last_year if len(last_year) == 2 else last_year
+    last_date = datetime.date(year=int(last_year), month=int(last_month), day=int(last_day))
+    _working_days = get_working_days(first_date, last_date)
+    _num_working_days = len(_working_days)
 
-	return _df, _working_days, _num_working_days
+    return _df, _working_days, _num_working_days
 
 
 def cli_questions(_df, _hours_by_project):
-	for index, row in _df.iterrows():
-		if row["Id Actividad"] == 92:
-			project = row["Proyecto"]
-			wp = int(row["Working Package"])
-			if wp != -1:
-				hours = input(f"Cuantas horas deseas imputar al WP {wp} del proyecto {project} (-1 si no hay mínimo): ")
-				_hours_by_project[project, wp] = int(hours)
-			else:
-				hours = input(f"Cuantas horas deseas imputar al proyecto {project} (-1 si no hay mínimo): ")
-				_hours_by_project[project, -1] = int(hours)
-	_teaching_hours = float(
-		input("Cuantas horas mínimas al día deseas imputar a la actividad de docencia (-1 si no hay mínimo): "))
+    for index, row in _df.iterrows():
+        if row["Id Actividad"] == 92:
+            project = row["Proyecto"]
+            wp = int(row["Working Package"])
+            if wp != -1:
+                hours = input(f"Cuantas horas deseas imputar al WP {wp} del proyecto {project} (-1 si no hay mínimo): ")
+                _hours_by_project[project, wp] = int(hours)
+            else:
+                hours = input(f"Cuantas horas deseas imputar al proyecto {project} (-1 si no hay mínimo): ")
+                _hours_by_project[project, -1] = int(hours)
+    _teaching_hours = float(
+        input("Cuantas horas mínimas al día deseas imputar a la actividad de docencia (-1 si no hay mínimo): "))
 
-	return _hours_by_project, _teaching_hours
+    return _hours_by_project, _teaching_hours
 
 
 def generate_questions(_df):
-	questions = {}
-	for index, row in _df.iterrows():
-		if row["Id Actividad"] == 92:
-			project = row["Proyecto"]
-			wp = int(row["Working Package"])
-			if wp != -1:
-				questions[project, wp] = f"Cuantas horas deseas imputar al WP {wp} del proyecto {project} (-1 si no hay mínimo): "
-			else:
-				questions[project, -1] = f"Cuantas horas deseas imputar al proyecto {project} (-1 si no hay mínimo): "
+    questions = {}
+    for index, row in _df.iterrows():
+        if row["Id Actividad"] == 92:
+            project = row["Proyecto"]
+            wp = int(row["Working Package"])
+            if wp != -1:
+                questions[project, wp] = f"Cuantas horas deseas imputar al WP {wp} del proyecto {project} (-1 si no hay mínimo): "
+            else:
+                questions[project, -1] = f"Cuantas horas deseas imputar al proyecto {project} (-1 si no hay mínimo): "
 
-	questions[
-		"__teaching__", -1] = "Cuantas horas mínimas al día deseas imputar a la actividad de docencia (-1 si no hay mínimo): "
+    if len(questions) == 0:
+        questions["__empty__", -1] = "No se han encontrado proyectos de investigación en el fichero CSV."
+    questions[
+        "__teaching__", -1] = "Cuantas horas mínimas al día deseas imputar a la actividad de docencia (-1 si no hay mínimo): "
 
-	return questions
+    return questions
 
 
 def solve_hours(_df, _hours_by_project, _working_days):
-	# iter dataframe by rows
-	for index, row in _df.iterrows():
-		project, wp = row['Proyecto'], row['Working Package']
-		if (project, wp) not in _hours_by_project:
-			continue
-		if _hours_by_project[(project, wp)] == -1:
-			continue
-		while _hours_by_project[(project, wp)] > 0:
-			for _day in _working_days:
-				pending_hours_in_day = MAX_HOUR_DAY - _df[_day].sum()
-				new_hours = min(min(1, _hours_by_project[(project, wp)]), pending_hours_in_day)
-				# update dataframe
-				_df.loc[index, _day] = _df.loc[index, _day] + new_hours
-				_hours_by_project[(project, wp)] -= new_hours
-	# projects without min hours restriction
-	projects_without_restriction = [key for key, value in _hours_by_project.items() if value == -1]
-	for _day in _working_days:
-		if len(projects_without_restriction) != 0:
-			project = random.choice(projects_without_restriction)
-			pending_hours_in_day = MAX_HOUR_DAY - _df[_day].sum()
-			# locate by two columns and row in df
-			_df.loc[(_df['Proyecto'] == project[0]) & (_df['Working Package'] == project[1]), _day] = pending_hours_in_day
+    # iter dataframe by rows
+    for index, row in _df.iterrows():
+        project, wp = row['Proyecto'], row['Working Package']
+        if (project, wp) not in _hours_by_project:
+            continue
+        if _hours_by_project[(project, wp)] == -1:
+            continue
+        while _hours_by_project[(project, wp)] > 0:
+            for _day in _working_days:
+                pending_hours_in_day = MAX_HOUR_DAY - _df[_day].sum()
+                new_hours = min(min(1, _hours_by_project[(project, wp)]), pending_hours_in_day)
+                # update dataframe
+                _df.loc[index, _day] = _df.loc[index, _day] + new_hours
+                _hours_by_project[(project, wp)] -= new_hours
+    # projects without min hours restriction
+    projects_without_restriction = [key for key, value in _hours_by_project.items() if value == -1]
+    for _day in _working_days:
+        if len(projects_without_restriction) != 0:
+            project = random.choice(projects_without_restriction)
+            pending_hours_in_day = MAX_HOUR_DAY - _df[_day].sum()
+            # locate by two columns and row in df
+            _df.loc[(_df['Proyecto'] == project[0]) & (_df['Working Package'] == project[1]), _day] = pending_hours_in_day
 
-		_df[_day] = _df[_day].apply(lambda x: str(x).replace('.', ',') if x != 0.0 else '')
+    return _df
 
-	return _df
+
+def transform_df_to_str_types(_df, _working_days):
+    for _day in _working_days:
+        _df[_day] = _df[_day].apply(lambda x: str(x).replace('.', ',') if x != 0.0 else '')
+    return _df
 
 
 def check_hours(_df, _working_days, _num_working_days, _teaching_hours, _hours_by_project):
-	used_hours = 0
-	# set df columns to float type with default value 0
-	for day in _working_days:
-		_df[day] = _df[day].apply(lambda x: float(str(x).replace(',', '.')))
-		_df[day] = _df[day].astype(float).fillna(0)
-		if _teaching_hours != -1:
-			# put in row "Docencia" and column day the max value between the current value and teaching_hours
-			_df.loc[_df['Id Actividad'] == 97, day] = _df.loc[_df['Id Actividad'] == 97, day].apply(lambda x: max(x, _teaching_hours))
+    used_hours = 0
+    # set df columns to float type with default value 0
+    for day in _working_days:
+        _df[day] = _df[day].apply(lambda x: float(str(x).replace(',', '.')))
+        _df[day] = _df[day].astype(float).fillna(0)
+        if _teaching_hours != -1:
+            # put in row "Docencia" and column day the max value between the current value and teaching_hours
+            _df.loc[_df['Id Actividad'] == 97, day] = _df.loc[_df['Id Actividad'] == 97, day].apply(
+                lambda x: max(x, _teaching_hours))
 
-		used_hours += _df[day].sum()
-	max_hours_month = MAX_HOUR_DAY * _num_working_days - used_hours
-	if sum([x for x in _hours_by_project.values() if x != -1]) > max_hours_month:
-		total_teaching_hours = _df.loc[_df['Id Actividad'] == 97, _df.columns[7:]].sum(axis=1)
-		msg = f"Te has pasado de horas.\nEl máximo es de 7.5h al día y {max_hours_month} horas este mes (sin contar la docencia).\n"
-		msg += f"Has asignado {total_teaching_hours.sum()} horas de docencia"
-		raise Exception(msg)
+        used_hours += _df[day].sum()
+    max_hours_month = MAX_HOUR_DAY * _num_working_days - used_hours
+    if sum([x for x in _hours_by_project.values() if x != -1]) > max_hours_month:
+        total_teaching_hours = _df.loc[_df['Id Actividad'] == 97, _df.columns[7:]].sum(axis=1)
+        msg = f"Te has pasado de horas.\nEl máximo es de 7.5h al día y {max_hours_month} horas este mes (sin contar la docencia).\n"
+        msg += f"Has asignado {total_teaching_hours.sum()} horas de docencia"
+        raise Exception(msg)
 
-	return _df
+    return _df
 
 
 if __name__ == '__main__':
-	# load csv to dataframe with iso-8859-1 encoding
-	filename = "/Users/jpalanca/Downloads/Horas_Participante.csv"
-	df, working_days, num_working_days = read_df(filename)
+    # load csv to dataframe with iso-8859-1 encoding
+    filename = "/Users/jpalanca/Downloads/Horas_Participante.csv"
+    df, working_days, num_working_days = read_df(filename)
 
-	hours_by_project = {}
+    hours_by_project = {}
 
-	hours_by_project, teaching_hours = cli_questions(df, hours_by_project)
+    hours_by_project, teaching_hours = cli_questions(df, hours_by_project)
 
-	df = check_hours(df, working_days, num_working_days, teaching_hours, hours_by_project)
+    df = check_hours(df, working_days, num_working_days, teaching_hours, hours_by_project)
 
-	df = solve_hours(df, hours_by_project, working_days)
+    df = solve_hours(df, hours_by_project, working_days)
 
-	df.to_csv('/Users/jpalanca/Downloads/output.csv', index=False, sep=";", encoding='iso-8859-1')
+    df = transform_df_to_str_types(df, working_days)
+
+    df.to_csv('/Users/jpalanca/Downloads/output.csv', index=False, sep=";", encoding='iso-8859-1')
